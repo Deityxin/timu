ddf3e19a848e3373d63d23dee14487e6eeef9088
柳鑫 <2015615229@qq.com>
2025-12-23 10:33:20 +0800

    chore: expand gitignore; update 07.cpp

--- bits/shiyan/07.cpp
+++ bits/shiyan/07.cpp
@@ -1,10 +1,5 @@
 #include <iostream>
-#include <sstream>
 using namespace std;
-
-int main() {
-    istringstream in("1 2");
-    int x, y; in >> x >> y;
-    cout << (x + y) << '\n';
-    return 0;
+int main (){
+    
 }
