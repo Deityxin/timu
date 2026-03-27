2c0751ab1724d7de3cc3fc8c95c244f8ecc31283
柳鑫 <2015615229@qq.com>
2026-03-27 21:00:03 +0800

    chore(auto): 更新题解 (2 files, +11/-11)
    Auto-generated summary
    scope: bits
    stats: 2 files, +11/-11
    
    changed files:
    - [M] bits/test/123.cpp
    - [M] bits/test/tempCodeRunnerFile.cpp

--- bits/test/123.cpp
+++ bits/test/123.cpp
@@ -3,14 +3,14 @@
 #include <string>
 using namespace std;
 vector<int> add(vector<int> &a,vector<int> &b){
-    if(a.size() < b.size()) return (b , a);
+    if(a.size() < b.size()) return add(b , a);
     int t = 0;
     vector<int> c;
     for(int i = 0; i < a.size() ; i++){
         t += a[i];
         if(i < b.size()) t = t + b[i];
-        c.push_back(t % 100);
-        t /= 100;
+        c.push_back(t % 10);
+        t /= 10;
     }
     if(t) c.push_back(t);
     return c;
@@ -18,12 +18,15 @@ vector<int> add(vector<int> &a,vector<int> &b){
 int main(){
     int n;
     cin >> n;
-    vector<int> tem(n + 1 , 0);
-    for(int i = 2 ; i <= n ; i++){
+    vector<vector<int>> tem(n + 1);
+    tem[1] = {1};
+    if (n >= 2) tem[2] = {2};
+    for(int i = 3 ; i <= n ; i++){
         tem[i] = add(tem[i - 2] , tem[i - 1]);
     }
-    if(n == 1) cout << 1;
-    else if(n == 2) cout << 2;
-    else cout << ;
+    vector<int> ans = tem[n];
+    for (int i = ans.size() - 1; i >= 0; i--) {
+        cout << ans[i];
+    }
     return 0;
 }
