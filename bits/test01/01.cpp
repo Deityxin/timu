0a79cda96e1f329db8fb25946f92448c567fbfab
柳鑫 <2015615229@qq.com>
2026-04-13 21:00:04 +0800

    chore(auto): 更新题解 (1 files, +48/-19)
    Auto-generated summary
    scope: bits
    stats: 1 files, +48/-19
    
    changed files:
    - [M] bits/test01/01.cpp

--- bits/test01/01.cpp
+++ bits/test01/01.cpp
@@ -1,22 +1,51 @@
 #include <iostream>
-#include <string>
-#include <vector>
 using namespace std;
-template <class T>
-bool panduan (T& a){
-    bool temp = false;
-    string s = to_string(a);
-    for(int i = 0; i < static_cast<int>(s.size()) / 2 ; i++){
-        if(s[i] != s[s.size() - 1 - i]) return temp;
+class Celestial {
+protected:
+    double m_mass;  
+    double m_radius; 
+public:
+    Celestial(double mass = 0.0, double radius = 0.0) 
+        : m_mass(mass), m_radius(radius) {
+        cout << "Celestial constructed\n";
     }
-    return temp = true;
-}
-bool panduan(const string& s) {
-    for (int i = 0; i < static_cast<int>(s.size()) / 2; ++i) {
-        if (s[i] != s[s.size() - 1 - i]) return false;
+    Celestial(const Celestial& other) 
+        : m_mass(other.m_mass), m_radius(other.m_radius) {
+        cout << "Celestial copy constructed\n";
     }
-    return true;
-}
-int main(){
+    Celestial& operator=(const Celestial& other) {
+        if (this == &other) return *this;
+        m_mass = other.m_mass;
+        m_radius = other.m_radius;
+        cout << "Celestial assigned\n";
+        return *this;
+    }
+    virtual ~Celestial() {
+        cout << "Celestial destructed\n";
+    }
+};
+class Planet : public Celestial {
+private:
+    Celestial m_satellite;
 
-}
+public:
+    Planet(double mass = 0.0, double radius = 0.0, 
+           const Celestial& satellite = Celestial()) 
+        : Celestial(mass, radius), m_satellite(satellite) {
+        cout << "Planet constructed\n";
+    }
+    Planet(const Planet& other) 
+        : Celestial(other), m_satellite(other.m_satellite) {
+        cout << "Planet copy constructed\n";
+    }
+    Planet& operator=(const Planet& other) {
+        if (this == &other) return *this;
+        Celestial::operator=(other);       
+        m_satellite = other.m_satellite;     
+        cout << "Planet assigned\n";
+        return *this;
+    }
+    virtual ~Planet() {
+        cout << "Planet destructed\n";
+    }
+};
